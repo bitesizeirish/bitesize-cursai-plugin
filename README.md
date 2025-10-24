da6fcbfd6144ed2628cb20ddc5e2b5cf0d985665
Eoin Ó Conchúir <e@eoinoc.net>
2025-10-24 14:58:25 +0100

    Initial commit: Basic WordPress plugin structure for Bitesize Cúrsaí

---
+++ README.md
@@ -0,0 +1,30 @@
+# Bitesize Cúrsaí Plugin
+
+A custom WordPress plugin for the Bitesize Irish Cúrsaí membership site.
+
+## About
+
+This plugin provides custom functionality and features for the Cúrsaí platform at [cursai.bitesize.irish](https://cursai.bitesize.irish), which offers Irish language lessons through a membership-based model.
+
+## Installation
+
+1. Upload the plugin files to the `/wp-content/plugins/bitesize-cursai-plugin` directory
+2. Activate the plugin through the 'Plugins' screen in WordPress
+
+## Development
+
+This plugin is under active development to support the Bitesize Irish Cúrsaí membership site.
+
+## Requirements
+
+- WordPress 5.0 or higher
+- PHP 7.4 or higher
+
+## License
+
+GPL v2 or later
+
+## Support
+
+For support, please contact Bitesize Irish.
+
---
+++ bitesize-cursai-plugin.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Plugin Name: Bitesize Cúrsaí
+ * Plugin URI: https://github.com/bitesizeirish/bitesize-cursai-plugin
+ * Description: Custom WordPress plugin for Bitesize Irish Cúrsaí membership site customizations
+ * Version: 1.0.0
+ * Author: Bitesize Irish
+ * Author URI: https://bitesize.irish
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: bitesize-cursai
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+/**
+ * Current plugin version.
+ */
+define('BITESIZE_CURSAI_VERSION', '1.0.0');
+
+/**
+ * Plugin directory path.
+ */
+define('BITESIZE_CURSAI_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+/**
+ * Plugin directory URL.
+ */
+define('BITESIZE_CURSAI_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+/**
+ * The code that runs during plugin activation.
+ */
+function activate_bitesize_cursai() {
+    // Activation code here
+}
+register_activation_hook(__FILE__, 'activate_bitesize_cursai');
+
+/**
+ * The code that runs during plugin deactivation.
+ */
+function deactivate_bitesize_cursai() {
+    // Deactivation code here
+}
+register_deactivation_hook(__FILE__, 'deactivate_bitesize_cursai');
+
+/**
+ * Begin execution of the plugin.
+ */
+function run_bitesize_cursai() {
+    // Plugin initialization code here
+}
+add_action('plugins_loaded', 'run_bitesize_cursai');
+
