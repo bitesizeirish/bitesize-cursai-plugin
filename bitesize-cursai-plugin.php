efd50f3a462561ac624b95727f8fb258435174f7
Eoin Ó Conchúir <e@eoinoc.net>
2025-10-24 15:15:40 +0100

    🚀 Deploy v1.0.3
    - Version bump to 1.0.3
    - Production build ready
    
    [auto-commit by deploy.sh]

--- bitesize-cursai-plugin.php
+++ bitesize-cursai-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Bitesize Cúrsaí
  * Plugin URI: https://github.com/bitesizeirish/bitesize-cursai-plugin
  * Description: Custom WordPress plugin for Bitesize Irish Cúrsaí membership site customizations
- * Version: 1.0.1
+ * Version: 1.0.3
  * Author: Bitesize Irish
  * Author URI: https://bitesize.irish
  * License: GPL v2 or later
@@ -21,7 +21,7 @@ if (!defined('WPINC')) {
  * Current plugin version.
  * Updated automatically by build script.
  */
-define('BITESIZE_CURSAI_VERSION', '1.0.1');
+define('BITESIZE_CURSAI_VERSION', '1.0.3');
 
 /**
  * Plugin directory path.
